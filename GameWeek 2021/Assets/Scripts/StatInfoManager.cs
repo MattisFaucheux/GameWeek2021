a1cde55c35f3eaf84eeaa9ef52b506230aaf8c9e
Mattis F <mattis.faucheux@gmail.com>
2021-04-15 14:41:19 -0400

    Add LvlUp System and IsartPrefab

--- GameWeek 2021/Assets/Scripts/StatInfoManager.cs
+++ GameWeek 2021/Assets/Scripts/StatInfoManager.cs
@@ -11,4 +11,70 @@ public class StatInfoManager : MonoBehaviour
     public StatInfo[] artStat = new StatInfo[4];
     public StatInfo[] artProf = new StatInfo[3];
     public StatInfo directeur;
+
+    public StatInfo GetStatInfo(enPlayerClass currentPlayerClass, int currentLvl)
+    {
+        switch (currentPlayerClass)
+        {
+            case enPlayerClass.GD:
+                return GetGDStat(currentLvl);
+
+            case enPlayerClass.GP:
+                return GetGPStat(currentLvl);
+
+            case enPlayerClass.ART:
+                return GetArtStat(currentLvl);
+
+            default:
+                return GetGDStat(currentLvl);
+        }
+    }
+
+    StatInfo GetGDStat(int currentLvl)
+    {
+        if (currentLvl < gdStat.Length)
+        {
+            return gdStat[currentLvl];
+        }
+        else if(currentLvl == gdStat.Length || (currentLvl > gdStat.Length && currentLvl < 5))
+        {
+            return gdProf[Random.Range(0, gdProf.Length)];
+        }
+        else
+        {
+            return directeur;
+        }
+    }
+
+    StatInfo GetGPStat(int currentLvl)
+    {
+        if (currentLvl < gpStat.Length)
+        {
+            return gpStat[currentLvl];
+        }
+        else if (currentLvl == gpStat.Length || (currentLvl > gpStat.Length && currentLvl < 5))
+        {
+            return gpProf[Random.Range(0, gpProf.Length)];
+        }
+        else
+        {
+            return directeur;
+        }
+    }
+
+    StatInfo GetArtStat(int currentLvl)
+    {
+        if (currentLvl < artStat.Length)
+        {
+            return artStat[currentLvl];
+        }
+        else if (currentLvl == artStat.Length || (currentLvl > artStat.Length && currentLvl < 5))
+        {
+            return artProf[Random.Range(0, artProf.Length)];
+        }
+        else
+        {
+            return directeur;
+        }
+    }
 }
