59922e2e2ea90f8cbee3ce3b02e805a8319e11f8
Mattis F <mattis.faucheux@gmail.com>
2021-04-14 22:02:45 -0400

    Add StatManager

---
+++ GameWeek 2021/Assets/Scripts/StatInfo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatInfo : MonoBehaviour
+{
+    public int strength;
+    public int moveSpeed;
+    public int maxStamina;
+    public int maxHealth;
+    public enPlayerClass playerClass;
+    public int currentLvl;
+    public int nbrKillLvlUp;
+    public Mesh mesh;
+}
---
+++ GameWeek 2021/Assets/Scripts/StatInfoManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatInfoManager : MonoBehaviour
+{
+    public StatInfo[] gdStat = new StatInfo[3];
+    public StatInfo[] gdProf = new StatInfo[3];
+    public StatInfo[] gpStat = new StatInfo[2];
+    public StatInfo[] gpProf = new StatInfo[3];
+    public StatInfo[] artStat = new StatInfo[4];
+    public StatInfo[] artProf = new StatInfo[3];
+    public StatInfo directeur;
+}
